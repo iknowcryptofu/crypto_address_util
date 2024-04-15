a1d2b93923b88af7a194683f5ce2d97e45841a89
Tim F <iknowcryptofu@gmail.com>
2024-04-15 22:04:31 +0100

    cleanup of showMatchedCurr function and formbs5.ejs

--- app.js
+++ app.js
@@ -21,42 +21,41 @@ if(valid)
 else
 	console.log('Address INVALID');
 
+
 // This will log 'This is a valid address' to the console.
 
 const btc_address = '1KFzzGtDdnq5hrwxXGjwVnKzRbvf8WVxck';
 const ada_address = 'addr1wysmmrpwphe0h6fpxlmcmw46frmzxz89yvpsf8cdv29kcnqsw3vw6';
 const eth_address = '0x42AE0B92E4985AD0Cde45e9c1Aa72e7f5A130B9E';
-var currencyNamesMatch = "";
+
 
 function showMatchedCurr(address) {
+    
+    var allCurrencies = WAValidator.getCurrencies();
 
-var allCurrencies = WAValidator.getCurrencies();
+    function checkCurrencies(currency) {
+        var valid =  WAValidator.validate(address, currency.symbol);
+        if (valid)
+            return currency;
+    };
 
-function checkCurrencies(currency) {
-	var valid =  WAValidator.validate(address, currency.symbol);
-	if (valid)
-		return currency;
-};
+    var currenciesMatch = allCurrencies.filter(checkCurrencies);
+    var currencyNamesMatch = currenciesMatch.map(token => token.name);
+    console.log(currencyNamesMatch);
 
-var currenciesMatch = allCurrencies.filter(checkCurrencies);
-//console.log(currenciesMatch);
-
-var currencyNamesMatch = currenciesMatch.map(a => a.name);
-console.log(currencyNamesMatch);
-//console.log(allCurrencies);
-return currencyNamesMatch;
+    return currencyNamesMatch;
 
 }
+
+//console.log(WAValidator.sayhi());
 
 // use res.render to load up an ejs view file
 var cryptoAddress = "abc";
 app.post("/", (req, res) => {
-
+    
     cryptoAddress = req.body.cryptoAddress;
-    //currencyNamesMatch = showMatchedCurr(cryptoAddress);
-    const password = req.body.password;
     console.log("Crypto Address: " + cryptoAddress);
-    //console.log("Password: " + password);
+    console.log(WAValidator.sayhi());
     //res.send("Data received");
     //to stay on same page
     //res.status(204).send();
@@ -64,25 +63,16 @@ app.post("/", (req, res) => {
     //res.redirect('/').render('/',{cryptoAddress:req.body.cryptoAddress});
     //res.render('pages/index', {cryptoAddress: "xyz"});
   });
+
 // index page 
 app.get('/', function(req, res) {
-    var mascots = [
-        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-        { name: 'Tux', organization: "Linux", birth_year: 1996},
-        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-    ];
-    var tagline = "No programming concept is complete without a cute animal mascot.";
-
+    
     currencyNamesMatch = showMatchedCurr(cryptoAddress);
 
     res.render('pages/index', {
-        mascots: mascots,
-        tagline: tagline,
-        //i added this
         cryptoAddress: cryptoAddress,
         currencyNamesMatch: currencyNamesMatch
-        
-    });
+        });
 });
 
 app.get("/indexcover3.html", (req, res) => {
