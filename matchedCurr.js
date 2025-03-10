ad64dfe8f81c99875e5605c4847b191dda16947f
Tim F <iknowcryptofu@gmail.com>
2025-03-10 20:41:09 +0000

    both matchedCurr fn refactored thx to llm

--- matchedCurr.js
+++ matchedCurr.js
@@ -1,56 +1,33 @@
-var WAValidator = require('trezor-address-validator2');
+const WAValidator = require('trezor-address-validator2');
 
 function showMatchedCurr(address) {
-    
-    var allCurrencies = WAValidator.getCurrencies();
-    var currAddrMatchMsg = "";
-    var noAddrFoundMsg = "";
-  
-    function checkCurrencies(currency) {
-        var valid =  WAValidator.validate(address, currency.symbol);
-        if (valid)
-            return currency;
-    };
+    const allCurrencies = WAValidator.getCurrencies();
 
-    var currenciesMatch = allCurrencies.filter(checkCurrencies);
-    var currencyNamesMatch = currenciesMatch.map(token => token.name);
-    
-    console.log(currencyNamesMatch);
-        
-    
-    if(currencyNamesMatch.length !== 0) {
-        var currAddrMatchMsg = `The address you gave matches ${currencyNamesMatch}.`;
-        currencyNamesMatch = currAddrMatchMsg;
-        
-    } else {
-        var noAddrFoundMsg = `The data you entered either doesn't match a crypto address that is on this website, or is not a valid address.`;
-        currencyNamesMatch = noAddrFoundMsg;
-    }
-    
-    return currencyNamesMatch;
+    // Filter and map currencies in one step for efficiency
+    const currencyNamesMatch = allCurrencies
+        .filter(currency => WAValidator.validate(address, currency.symbol))
+        .map(currency => currency.name);
 
+    // Ternary operator for concise conditional logic
+    return currencyNamesMatch.length > 0
+        ? `The address you gave matches ${currencyNamesMatch.join(', ')}.`
+        : `The data you entered either doesn't match a crypto address that is on this website, or is not a valid address.`;
 }
 
+
 function showMatchedCurr2(address) {
-    
-    var allCurrencies = WAValidator.getCurrencies();
-    var currAddrMatchMsg = "";
-    var noAddrFoundMsg = "";
-  
-    function checkCurrencies(currency) {
-        var valid =  WAValidator.validate(address, currency.symbol);
-        if (valid)
-            return currency;
-    };
+    const allCurrencies = WAValidator.getCurrencies();
 
-    var currenciesMatch = allCurrencies.filter(checkCurrencies);
-    var currencyNamesMatch = currenciesMatch.map(token => token.name);
-    
+    // Filter and map currencies in one step for efficiency
+    const currencyNamesMatch = allCurrencies
+        .filter(currency => WAValidator.validate(address, currency.symbol))
+        .map(currency => currency.name);
+
+    // Log the matched currency names
     console.log(currencyNamesMatch);
-        
-    
-    return currencyNamesMatch;
 
+    // Return the matched currency names array directly
+    return currencyNamesMatch;
 }
 
 module.exports = { showMatchedCurr, showMatchedCurr2 }
