c7d0d5611b8d96561f34f85a2afdf47a9494ff4a
Tim F <iknowcryptofu@gmail.com>
2025-03-11 20:39:29 +0000

    common function for matchedCurr added. from llm.

--- matchedCurr.js
+++ matchedCurr.js
@@ -1,33 +1,27 @@
 const WAValidator = require('trezor-address-validator2');
 
-function showMatchedCurr(address) {
+// Common function to get matched currency names for a given address
+function getMatchedCurrencyNames(address) {
     const allCurrencies = WAValidator.getCurrencies();
-
-    // Filter and map currencies in one step for efficiency
-    const currencyNamesMatch = allCurrencies
+    return allCurrencies
         .filter(currency => WAValidator.validate(address, currency.symbol))
         .map(currency => currency.name);
+}
 
-    // Ternary operator for concise conditional logic
+// Function to return a formatted message based on matched currencies
+function showMatchedCurr(address) {
+    const currencyNamesMatch = getMatchedCurrencyNames(address);
+
     return currencyNamesMatch.length > 0
         ? `The address you gave matches ${currencyNamesMatch.join(', ')}.`
         : `The data you entered either doesn't match a crypto address that is on this website, or is not a valid address.`;
 }
 
-
+// Function to log and return matched currency names directly
 function showMatchedCurr2(address) {
-    const allCurrencies = WAValidator.getCurrencies();
-
-    // Filter and map currencies in one step for efficiency
-    const currencyNamesMatch = allCurrencies
-        .filter(currency => WAValidator.validate(address, currency.symbol))
-        .map(currency => currency.name);
-
-    // Log the matched currency names
+    const currencyNamesMatch = getMatchedCurrencyNames(address);
     console.log(currencyNamesMatch);
-
-    // Return the matched currency names array directly
     return currencyNamesMatch;
 }
 
-module.exports = { showMatchedCurr, showMatchedCurr2 }
+module.exports = { showMatchedCurr, showMatchedCurr2 };
